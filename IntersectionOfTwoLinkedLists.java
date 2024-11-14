7feb8b1f40cd9836192c5fb11e006c331635fa9e
MSP <110626902+MSP07@users.noreply.github.com>
2024-11-15 00:58:06 +0530

    Create IntersectionOfTwoLinkedLists.java

---
+++ IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,21 @@
+/*approach
+  linked list
+  we keep two pointers one at headA and another at headB we iterate on one node at 
+  a time in both pointers at any point both nodes are equal that is the intersection point
+  else if either of them reaches the end we switch their places like is pointer1 reaches null
+  we make pointer1 = headB and vice versa this approach works because this handles the case for
+  every linked list length*/
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curr1 = headA;
+        ListNode curr2 = headB;
+        while(curr1!=curr2){
+            curr1 = curr1.next;
+            curr2 = curr2.next;
+            if(curr1==curr2)return curr1;
+            if(curr1==null)curr1 = headB;
+            if(curr2==null)curr2 = headA;
+        }
+        return curr2;
+    }
+}
