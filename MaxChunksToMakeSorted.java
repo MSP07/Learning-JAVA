9276ea718a16a62aa737a98b3c06338332a74b64
MSP <110626902+MSP07@users.noreply.github.com>
2024-12-20 23:42:31 +0530

    Create MaxChunksToMakeSorted.java

---
+++ MaxChunksToMakeSorted.java
@@ -0,0 +1,18 @@
+/*approach
+  linear search
+  in order to find the chunks that will make up min sorting
+  we just find the highest element comapred to prev element
+  and if the highest element is in the same index then from
+  [0, index] becomes a chunk so we incr the chunks to be sorted
+  and finally return the count*/
+class Solution{
+    public int maxChunksToSort(int[] arr){
+        int n = arr.length;
+        int chunks = 0, maxi = 0;
+        for(int i=0;i<n;i++){
+            maxi = Math.max(maxi,arr[i]);
+            if(maxi==i)++chunks;
+        }
+        return chunks;
+    }
+}
