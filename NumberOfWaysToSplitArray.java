d34a315768a56274a3b7b6409a008facdce4f6a2
MSP <110626902+MSP07@users.noreply.github.com>
2025-01-03 23:19:40 +0530

    Create NumberOfWaysToSplitArray.java

---
+++ NumberOfWaysToSplitArray.java
@@ -0,0 +1,23 @@
+/*approach
+  prefixSum
+  since our aim is to find points where leftSum >= rightSum
+  first in rightSum we find the total of all elements and then
+  starting from left till the last before element we simultaneosuly
+  add the element to leftsum and remove it from total and whenever 
+  leftsum >= rightsum there is a point so we increase the splits/points
+  and finally return it*/
+class Solution{
+    public int waysToSplitArray(int[] nums){
+        long leftSum = 0, rightSum = 0;
+        for(int num:nums){
+            rightSum += num;
+        }
+        int splits = 0;
+        for(int i=0;i<nums.length-1;i++){
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            if(leftSum >= rightSum)splits++;
+        }
+        return splits;
+    }
+}
