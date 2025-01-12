71dd49be6cb2925ebbd9ae2b6b2184cb98faff8e
MSP <110626902+MSP07@users.noreply.github.com>
2025-01-13 03:08:33 +0530

    Create CheckIfAParanthesisStringCanBeValid.java

---
+++ CheckIfAParanthesisStringCanBeValid.java
@@ -0,0 +1,44 @@
+/*approach
+  string, counting, greedy
+  to find the string is valid the no of open and closed brackets should match
+  if string is odd length then it is not possible. Additionally if locked[i]=='1'
+  then we can't change the char so we take the count as it is while iterating
+  we iterate from left to right taking open bracket counts if the locked[i]=='1'
+  then open++ for'(' else open-- if locked[i]=='0' then we can change the brackets to
+  our wish so we take the count as per our wish and incr the open count. Then we iterate
+  from right to left this time we follow the same but with closed brackets and at any time
+  while these iterations if either of the count is < 0 then we return false else finally
+  we return true*/
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int n = s.length();
+        if(n%2!=0)return false;
+        int open_cnt = 0;
+        for(int i=0;i<n;i++){
+            if(locked.charAt(i)=='1'){
+                if(s.charAt(i)=='('){
+                    open_cnt++;
+                }else{
+                    open_cnt--;
+                }
+            }else{
+                open_cnt++;
+            }
+            if(open_cnt<0)return false;
+        }
+        int closed_cnt = 0;
+        for(int i=n-1;i>=0;i--){
+            if(locked.charAt(i)=='1'){
+                if(s.charAt(i)==')'){
+                    closed_cnt++;
+                }else{
+                    closed_cnt--;
+                }
+            }else{
+                closed_cnt++;
+            }
+            if(closed_cnt<0)return false;
+        }
+        return true;
+    }
+}
