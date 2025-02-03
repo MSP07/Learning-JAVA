aea12f6a49be930cb1818ee087506f62958f8d12
MSP <110626902+MSP07@users.noreply.github.com>
2025-02-04 01:13:03 +0530

    Create LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.java

---
+++ LongestStrictlyIncreasingOrStrictlyDecreasingSubarray.java
@@ -0,0 +1,27 @@
+/*approach
+  array,counting
+  by default the length of increasing and decreasing sequence is 1
+  so when we encounter a increasing sequence we assume its strictly increasing and set
+  the decr count to one and we do the vice versa when we encounter a decreasing sequence and
+  when elements are equal we just reset them to their org count and keep track of max ones 
+  and finally return the max one*/
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int incr = 1, decr = 1;
+        int maxi = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                incr++;
+                decr = 1;
+            }else if(nums[i]>nums[i-1]){
+                decr++;
+                incr = 1;
+            }else{
+                incr = 1;
+                decr = 1;
+            }
+            maxi = Math.max(maxi,Math.max(incr,decr));
+        }
+        return maxi;
+    }
+}
