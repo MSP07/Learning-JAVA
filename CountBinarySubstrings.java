77b40eb40758d02d31691c54a3214fdef1534b65
MSP <110626902+MSP07@users.noreply.github.com>
2025-03-01 17:06:50 +0530

    Create CountBinarySubstrings.java

---
+++ CountBinarySubstrings.java
@@ -0,0 +1,21 @@
+/*approach
+  string
+  we simply keep track of the count of same chars 0's or 1's
+  when the streak breaks when s[i-1] != s[i] then we add the min of
+  1's count and 0's count to the ans and keep the streak curr as 1 again
+  else we incr the curr and finally return the ans with min of prev,curr*/
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ans = 0, prev = 0, curr = 1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i-1)!=s.charAt(i)){
+                ans += Math.min(prev,curr);
+                prev = curr;
+                curr = 1;
+            }else{
+                curr++;
+            }
+        }
+        return ans + Math.min(prev,curr);
+    }
+}
