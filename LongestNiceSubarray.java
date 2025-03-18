adc576033f432b7e2f72744627ce733919e6d25a
MSP <110626902+MSP07@users.noreply.github.com>
2025-03-19 01:35:31 +0530

    Create LongestNiceSubarray.java

---
+++ LongestNiceSubarray.java
@@ -0,0 +1,20 @@
+/*approach
+  bit manipulation, sliding window
+  first we have used = 0, we start and from left =0 
+  while used^nums[right] != 0, we perform ans with used ^= nums[left]
+  shrink it to left,right then used |= nums[right] and update max with max(maxi,right-left+1)
+  finally return the maxi*/
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int used = 0, left = 0, maxi = 0;
+        for(int right=0;right<nums.length;right++){
+            while((used&nums[right])!=0){
+                used ^= nums[left];
+                left++;
+            }
+            used |= nums[right];
+            maxi = Math.max(maxi,right-left+1);
+        }
+        return maxi;
+    }
+}
